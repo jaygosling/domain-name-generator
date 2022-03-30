12fdb08db15cf256f335035751a510ee49f3e215
jaygosling <javijimenezclemente@gmail.com>
2022-03-30 16:21:26 +0000

    Version 1.0

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let article = ["our", "my", "your", "a", "their"];
+let adjective = [
+  "bloody",
+  "big",
+  "great",
+  "own",
+  "fantastic",
+  "amazing",
+  "small",
+  "tiny",
+  "obnoxious",
+  "terrible"
+];
+let noun = ["house", "universe", "car", "project", "cat"];
+let domain = [
+  ".com",
+  ".us",
+  ".eu",
+  ".es",
+  ".wtf",
+  ".de",
+  ".fr",
+  ".co.uk",
+  ".ie",
+  ".cat"
+];
+let button = document.querySelector("#genButton");
+let domainName = document.querySelector("#domName");
+button.addEventListener("click", function() {
+  let art = article[Math.floor(Math.random() * 5)];
+  let adj = adjective[Math.floor(Math.random() * 10)];
+  let nouns = noun[Math.floor(Math.random() * 5)];
+  let dom = domain[Math.floor(Math.random() * 10)];
+  domainName.innerHTML = art + adj + nouns + dom;
+});
